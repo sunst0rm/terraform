b983700775aeae176c694843d1faf1c78777c486
sunst0rm <jarek213@gmail.com>
2023-03-08 12:15:35 +0100

    modified provider version - version constraints section

--- local-file/local.tf
+++ local-file/local.tf
@@ -1,3 +1,13 @@
+terraform {
+    required_providers {
+        local = {
+            source = "hashicorp/local"
+            version = "1.4.0"
+        }
+    }
+}
+
+
 resource "local_file" "pet" {
     filename = var.filename
     content = var.content
